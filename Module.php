5817240225eded922fde34b662bd972da7655b07
afterlogic <support@afterlogic.com>
2020-06-17 14:09:00 +0300

    some improvements

--- Module.php
+++ Module.php
@@ -20,52 +20,18 @@ class Module extends \Aurora\System\Module\AbstractModule
 {
 	public function init()
 	{
-//		$this->subscribeEvent('Core::Login::after', array($this, 'onAfterLogin'), 10);
-		$this->subscribeEvent('Core::Authenticate::after', array($this, 'onAfterAuthenticate'), 10);
+		$this->subscribeEvent('Core::SetAuthDataAndGetAuthToken::after', array($this, 'onAfterAuthenticate'), 10);
 	}
 
 	public function onAfterAuthenticate(&$aArgs, &$mResult)
 	{
-		if ($mResult && is_array($mResult) && isset($mResult['token']))
+		if ($mResult && is_array($mResult) && isset($mResult['AuthToken']))
 		{
-			$oUser = \Aurora\System\Api::getUserById((int) $mResult['id']);
-			if ($oUser instanceof \Aurora\Modules\Core\Classes\User && $oUser->isNormalOrTenant())
+			$sXClientHeader = (string) \MailSo\Base\Http::SingletonInstance()->GetHeader('X-Client');
+
+			if (strtolower($sXClientHeader) !== 'webclient')
 			{
-				$sXClientHeader = (string) \MailSo\Base\Http::SingletonInstance()->GetHeader('X-Client');
-
-				if (strtolower($sXClientHeader) !== 'webclient')
-				{
-					$mResult['AllowAccess'] = 1;
-				}
-			}
-		}
-	}
-
-	public function onAfterLogin(&$aArgs, &$mResult)
-	{
-		if($mResult && isset($mResult['AuthToken'])) {
-			$oAuthenticatedUser = \Aurora\System\Api::getAuthenticatedUser();
-			if ($oAuthenticatedUser instanceof \Aurora\Modules\Core\Classes\User
-					&& $oAuthenticatedUser->isNormalOrTenant()
-					&& $this->isUserNotFromBusinessTenant($oAuthenticatedUser)
-					// && isset($aArgs['EntryName'])
-					// && strtolower($aArgs['EntryName']) === 'api'
-				)
-			{
-				$sXClientHeader = (string) \MailSo\Base\Http::SingletonInstance()->GetHeader('X-Client');
-				$bAllowMobileApps = $this->getGroupSetting($oAuthenticatedUser->EntityId, 'AllowMobileApps');
-				if (strtolower($sXClientHeader) !== 'webclient' && (!is_bool($bAllowMobileApps) || !$bAllowMobileApps))
-				{
-					throw new \Aurora\System\Exceptions\ApiException(
-						\Aurora\System\Notifications::AccessDenied,
-						null,
-						$this->i18N('ERROR_USER_MOBILE_ACCESS_LIMIT'),
-						[],
-						$this
-					);
-
-					return true;
-				}
+				$mResult['AllowAccess'] = 1;
 			}
 		}
 	}
